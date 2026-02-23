def9b9f08d8d9ec6aaa154b7fda866f65d5b78ac
aceecahub <itecot02@gmail.com>
2026-02-23 11:06:07 +0700

    add API public

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -12,7 +12,7 @@ export default function SignUp() {
 
   const handleLogin = async (e: React.FormEvent) => {
     e.preventDefault();
-    setError(""); 
+    setError("");
     setLoading(true);
 
     try {
